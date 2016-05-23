c464f2ad1058f12a384ebea30d5cc031ea26f7e6
aumgn <aumgn@free.fr>
2016-05-23 09:12:46 +0200

    Add prefix to EngineLoggerObserver

--- src/main/java/engine/EngineLoggerObserver.java
+++ src/main/java/engine/EngineLoggerObserver.java
@@ -14,14 +14,20 @@ import static java.util.stream.Collectors.toList;
 public class EngineLoggerObserver implements EngineObserver {
 
     private final Engine engine;
+    private final String prefix;
 
     public EngineLoggerObserver(Engine engine) {
+        this(engine, "");
+    }
+
+    public EngineLoggerObserver(Engine engine, String prefix) {
         this.engine = engine;
+        this.prefix = prefix.endsWith(" ") ? prefix : prefix + " ";
     }
 
     @Override
     public void onStart() {
-        engine.logger().severe("Engine starting with seed {0}", Long.toString(engine.getSeed()));
+        engine.logger().severe("{0}Engine starting with seed {1}", prefix, Long.toString(engine.getSeed()));
     }
 
     @Override
@@ -30,7 +36,8 @@ public class EngineLoggerObserver implements EngineObserver {
 
     @Override
     public void onTileStepStart(boolean cancelled) {
-        engine.logger().info("* Turn {0} {1} {2}-{3} ({4} tiles remaining)",
+        engine.logger().info("{0}* Turn {1} {2} {3}-{4} ({5} tiles remaining)",
+                prefix,
                 engine.getStatus().getTurn(),
                 engine.getCurrentPlayer().getColor(),
                 engine.getVolcanoTileStack().current().getLeft(),
@@ -44,12 +51,13 @@ public class EngineLoggerObserver implements EngineObserver {
 
     @Override
     public void onTilePlacementOnSea(SeaTileAction placement) {
-        engine.logger().info("  Placed on sea {0} {1}", placement.getHex1(), placement.getOrientation());
+        engine.logger().info("{0}  Placed on sea {1} {2}", prefix, placement.getHex1(), placement.getOrientation());
     }
 
     @Override
     public void onTilePlacementOnVolcano(VolcanoTileAction placement) {
-        engine.logger().info("  Placed on volcano {0} {1} at level {2}",
+        engine.logger().info("{0}  Placed on volcano {1} {2} at level {3}",
+                prefix,
                 placement.getVolcanoHex(),
                 placement.getOrientation(),
                 engine.getIsland().getField(placement.getVolcanoHex()).getLevel());
@@ -57,20 +65,23 @@ public class EngineLoggerObserver implements EngineObserver {
 
     @Override
     public void onBuild(PlaceBuildingAction action) {
-        engine.logger().info("  Built a {0} at {1}", action.getType(), action.getHex());
+        engine.logger().info("{0}  Built a {1} at {2}", prefix, action.getType(), action.getHex());
         logRemainingBuilding();
     }
 
     @Override
     public void onExpand(ExpandVillageAction action) {
-        engine.logger().info("  Expanded a village at {0} towards {1}",
-                action.getVillageHex(), action.getFieldType());
+        engine.logger().info("{0}  Expanded a village at {1} towards {2}",
+                prefix,
+                action.getVillageHex(),
+                action.getFieldType());
         logRemainingBuilding();
     }
 
     private void logRemainingBuilding() {
         for (Player player : engine.getPlayers()) {
-            engine.logger().info("  [{0}] Hut({1}) Temple({2}) Tower({3})",
+            engine.logger().info("{0}  [{1}] Hut({2}) Temple({3}) Tower({4})",
+                    prefix,
                     Strings.padEnd(player.getColor().toString(), 6, ' '),
                     player.getBuildingCount(BuildingType.HUT),
                     player.getBuildingCount(BuildingType.TEMPLE),
@@ -80,12 +91,13 @@ public class EngineLoggerObserver implements EngineObserver {
 
     @Override
     public void onEliminated(Player eliminated) {
-        engine.logger().info("!!! Eliminated: {0} !!!", eliminated.getColor());
+        engine.logger().info("{0}!!! Eliminated: {1} !!!", prefix, eliminated.getColor());
     }
 
     @Override
     public void onWin(EngineStatus.FinishReason reason, List<Player> winners) {
-        engine.logger().info("!!! Winner(s): {0} ({1}) !!!",
+        engine.logger().info("{0}!!! Winner(s): {1} ({2}) !!!",
+                prefix,
                 winners.stream().map(Player::getColor).collect(toList()),
                 reason);
     }
