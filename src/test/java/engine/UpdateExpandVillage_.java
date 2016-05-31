17dc669ff59b6d2621577c6dc645849573754e6c
aumgn <aumgn@free.fr>
2016-05-31 08:14:50 +0200

    Uppercase

--- src/test/java/engine/UpdateExpandVillage_.java
+++ src/test/java/engine/UpdateExpandVillage_.java
@@ -23,7 +23,7 @@ import java.util.logging.Level;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
-public class updateExpandVillage {
+public class UpdateExpandVillage_ {
 
     private Set<ExpandVillageAction> getExpandVillageActionsUnique(Engine engine) {
         Set<ExpandVillageAction> actual = new HashSet<>();
