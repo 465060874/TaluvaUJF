05f13c5c7db6b8f83131c50c2f069b1c6415491d
Boyan-MILANOV <boyan.milanov@hotmail.fr>
2016-06-02 11:30:16 +0200

    MOTEUR - Bugs en partie corrigés

--- src/main/java/engine/tilestack/VolcanoTileStack.java
+++ src/main/java/engine/tilestack/VolcanoTileStack.java
@@ -2,8 +2,12 @@ package engine.tilestack;
 
 import com.google.common.io.CharSink;
 import com.google.common.io.CharSource;
+import com.google.common.io.Files;
+import data.StandardVolcanoTiles;
 import data.VolcanoTile;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
 /**
@@ -40,6 +44,14 @@ public interface VolcanoTileStack {
     interface Factory {
 
         VolcanoTileStack create(int count, Random random);
+    }
+
+    static void main(String[] args) {
+        Factory factory = VolcanoTileStack.randomFactory(StandardVolcanoTiles.LIST);
+        VolcanoTileStack tileStack = factory.create(24, new Random());
+
+        File file = new File("stack");
+        tileStack.saveAll(Files.asCharSink(file, StandardCharsets.UTF_8));
     }
 }
 
